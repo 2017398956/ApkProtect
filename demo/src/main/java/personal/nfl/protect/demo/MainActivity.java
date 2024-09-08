456a910d02bf7e478f43c3a9188d12866e033b3c
2017398956 <2017398956@qq.com>
2024-09-08 21:55:00 +0800

    替换 res_id

--- demo/src/main/java/personal/nfl/protect/demo/MainActivity.java
+++ demo/src/main/java/personal/nfl/protect/demo/MainActivity.java
@@ -10,8 +10,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        int[] appCompatTheme = R.styleable.AppCompatTheme;
+        int appCompatThemeWindowActionBar = R.styleable.AppCompatTheme_windowActionBar;
         setContentView(R.layout.activity_main);
-
         findViewById(R.id.btn_jump).setOnClickListener(v -> {
             Intent intent = new Intent(this, SecondActivity.class);
             startActivity(intent);
